629a1c90def5182ddc01f1356d80cbde692635c7
alpolcaymis <alpmis99@gmail.com>
2023-11-28 19:37:29 +0300

    async-await 1

---
+++ callback.js
@@ -0,0 +1,13 @@
+// setTimeout(() => {
+//   console.log("merhaba");
+// }, 1000);
+
+// setInterval(() => {
+//   console.log("ben her saniye çalışacağım.");
+// }, 1000);
+
+const merhabaDe = () => {
+  console.log("merhaba");
+};
+
+merhabaDe();
