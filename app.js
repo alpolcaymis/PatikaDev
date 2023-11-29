a0491b32e17648f65df2527c8b73868ea76067a3
alpolcaymis <alpmis99@gmail.com>
2023-11-29 16:03:46 +0300

    user_id > 1 koşulu için edited

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 async function getData(user_id) {
+  // fetch data from api url || axios ile datamızı aldık
+  // assign fetched data as the "user" || gelen datayı "user" olarak atıyoruz, saklıyoruz.
   const { data: user } = await axios(
     "https://jsonplaceholder.typicode.com/users/" + user_id
   );
 
+  // fetch data from api url || axios ile datamızı aldık
+  // assign fetched data as "postsOfUser" || gelen datayı "postsOfUser" olarak atıyoruz, saklıyoruz.
   const { data: postsOfUser } = await axios(
     // `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
-    `https://jsonplaceholder.typicode.com/posts?userId=${user_id}&&id=${user_id}`
+
+    // aşağıdaki if statement, tüm user_id inputları için yanlızca tek bir ve ilk postu output etmemizi sağlayan bir çözüm.
+    `https://jsonplaceholder.typicode.com/posts?userId=${user_id}&&id=${user_id}${
+      user_id > 1 ? 0 : ""
+    }`
   );
-
-  // console.log(`===========> infos_of_user_${user_id}`, user);
-  // console.log(`===========> posts_of_user_${user_id}`, postsOfUser);
-  // console.log({ user, posts: postsOfUser });
-  // console.log({ user, posts: postsOfUser });
-
-  // let mergedObject = { ...user, ...postsOfUser };
-  // return mergedObject;
 
   console.log({ ...user, posts: postsOfUser });
 
+  //Spread operator objemizi kopyalamayı sağlıyor ve ödevdeki görünümün aynısını elde etmeyi başarıyoruz.
   return { ...user, posts: postsOfUser };
 }
 export default getData;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import getData from "./app.js";
 
 getData(1);
+// getData(2);
+// getData(3);
