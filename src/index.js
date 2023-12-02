45ca664bb6425037c4033793e5035d5036fb1aea
alpolcaymis <alpmis99@gmail.com>
2023-12-02 18:54:37 +0300

    son hali bence mükemmel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Bunu derste görmüştük, DOM yüklendikten sonra tüm JS çalışacak. defer async falan
 document.addEventListener("DOMContentLoaded", () => {
-  // Element'leri önce elime alıyorum
+  // Önce Element'leri elime alıyorum
   // Form Container
   const form = document.getElementById("form");
   const titleInput = document.getElementById("title");
@@ -9,43 +9,45 @@ document.addEventListener("DOMContentLoaded", () => {
   const loadingDiv = document.getElementById("loading");
   const list = document.getElementById("list");
 
-  // API'den verileri çekiyoruz ve listele
+  // API'den verileri çekiyoruz
   fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
     .then((response) => response.json())
     .then((data) => {
-      loadingDiv.style.display = "none";
       data.forEach((fetchedTaskObject) => {
         console.log(fetchedTaskObject);
+        // Objemi listeye ekliyorum
         appendToList(fetchedTaskObject);
       });
-    });
+    })
+    // Sonra Loading Div'ini görünmez yapıyorum, aslında hala DOM'da duruyor.
+    .then((loadingDiv.style.display = "none"));
 
-  // Form submit edilince
-  form.addEventListener("submit", (e) => {
-    e.preventDefault(); //reflesh önlüyor
-
-    // Task Object'imi oluşturuyorum...
-    // Form'dan gelenleri alıyorum objeye koyuyorum task = {key : value, key : value};
-    const newTaskObject = {
-      title: titleInput.value,
-      completed: completedInput.checked,
-    };
-
-    // Listeye ekliyorum, yeni objemi yollayarak.
-    appendToList(newTaskObject);
-
-    // Formu temizliyorum
-    form.reset();
-  });
-
-  // <ul> Liste içine yeni Task List Item ekle
   function appendToList(taskObject) {
     const taskListItem = document.createElement("li"); // li koydum
     taskListItem.classList.add("list-item"); // li style ismi ekledim
     taskListItem.innerHTML = `                        
                     <strong>${taskObject.title}</strong>
-                    <span>${taskObject.completed ? "✔" : "❌"}</span>
+                    <span>${taskObject.completed ? "✅" : "❌"}</span>
                 `;
+    // <ul> içine yolluyorum
     list.appendChild(taskListItem);
   }
+
+  // Form submit edilince
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); //refresh'i önlüyor
+
+    // Task Object'imi oluşturuyorum...
+    // Form'dan gelenleri alıyorum objeye koyuyorum. task = {key : value, key : value};
+    const newTaskObject = {
+      title: titleInput.value,
+      completed: completedInput.checked,
+    };
+
+    // Objemi listeye ekliyorum
+    appendToList(newTaskObject);
+
+    // Formu temizliyorum
+    form.reset();
+  });
 });
