c435cd833402ae016505385cd89cb2c9a91b6ec7
alpolcaymis <alpmis99@gmail.com>
2023-12-14 15:56:41 +0300

    Start Point

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Contacts from "./components/Contacts";
 
 function App() {
   return (
     <div id="container">
-      <header>
-        <img src="../public/logo.png" alt="" />
-        <h1>
-          Your <em>Personal</em> Contact Book
-        </h1>
-        <p>Keep your contacts organized and up to date</p>
-      </header>
-      <Contacts />
+      <h1>HEllo WolrD!</h1>
+      <p>Wwather Applicaiont</p>
     </div>
   );
 }
