75b3c5036aadd4ffa2dd97fed65f1dd324437f6d
alpolcaymis <alpmis99@gmail.com>
2023-12-17 21:45:39 +0300

    finish lanet proje

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import axios from "axios";
+import { Button } from "test-button-alp";
 import "./index.css";
 export default function App() {
   return (
     <div className="w-full h-full relative">
       <div className="text-center p-4">
-        <input
-          type="text"
-          className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-sky-300 
-          shadow-md"
-          placeholder="Enter Location"
-        />
+        <Button type="primary">Selam</Button>
+        <Button type="default">Selam</Button>
+        <Button type="dashed">Selam</Button>
+        <Button type="text">Selam</Button>
+        <Button type="link">Selam</Button>
       </div>
     </div>
   );
