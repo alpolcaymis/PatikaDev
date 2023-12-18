fe1f59482493ac1fc672ebe922dffcd7b9fabbc9
alpolcaymis <alpmis99@gmail.com>
2023-12-18 22:48:48 +0300

    asf

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import product_IMG from "../assets/p_IMG.jpg";
 
-const Product = ({ title }) => {
+const Product = ({ item, filterArray }) => {
   return (
-    <li
-      className="border-2 border-yellow-300 
-  w-[200px] min-h-fit  overflow-hidden p-2 "
-    >
-      <img src={product_IMG} alt="" className="mx-auto w-full" />
-      <h2>{title}</h2>
-      <div>
-        price :<b>100 €</b>
-      </div>
-      <div className="mt-2 relative">
-        <button className="px-4 py-1 bg-slate-50 text-slate-900 border-slate-900 border-2 ">
-          Edit
-        </button>
-        <button className="border-2 px-2 py-1 bg-slate-800 text-slate-50 absolute right-0">
-          Delete
-        </button>
-      </div>
-    </li>
+    <>
+      {item && (
+        <li
+          className="border-2 border-yellow-300 
+      w-[200px] min-h-fit  overflow-hidden p-2 "
+        >
+          <img src={item.image} alt="" className="mx-auto w-full" />
+          <h2>{item.title}</h2>
+          <div>
+            price : <b>{item.price} €</b>
+          </div>
+          <div className="mt-2 relative">
+            <button className="px-4 py-1 bg-slate-50 text-slate-900 border-slate-900 border-2 ">
+              Edit
+            </button>
+            <button
+              className="border-2 px-2 py-1 bg-slate-800 text-slate-50 absolute right-0"
+              onClick={() => filterArray(item.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      )}
+    </>
   );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,16 +3,26 @@ import Product from "./Product";
 import { useCartContext } from "../context/cart-context";
 
 const ProductList = () => {
-  const { productsArray } = useCartContext();
+  const { productsArray, setProductsArray } = useCartContext();
 
   console.log("selam", productsArray);
+
+  const filterArray = (productID) => {
+    const filteredArray = productsArray.filter((productID) => {
+      productID !== productsArray.id;
+    });
+    setProductsArray(filteredArray);
+    console.log("filteredArray", filteredArray);
+  };
 
   return (
     <div className="basis-10/12 border-4 border-red-900 ">
       <h1 className="text-center">ProductList</h1>
       <ul className=" flex flex-wrap gap-4 p-8 justify-center md:justify-start ">
         {productsArray.map((item) => {
-          <li>{item.id}</li>;
+          return (
+            <Product key={item.id} item={item} filterArray={filterArray} />
+          );
         })}
         <Product />
       </ul>
