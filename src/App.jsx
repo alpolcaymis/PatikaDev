763aeeb4835c81766460c121b9ac53167931f9a4
alpolcaymis <alpmis99@gmail.com>
2024-01-03 16:24:04 +0300

    re-ordered

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Header from "./components/Header";
+
+import { CartContextProvider } from "./context/cart-context";
 import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import NewTicket from "./pages/NewTicket";
-import Tickets from "./pages/Tickets";
 import Ticket from "./pages/Ticket";
-import { CartContextProvider } from "./context/cart-context";
+import Tickets from "./pages/Tickets";
+import NewTicket from "./pages/NewTicket";
 import AdminPanel from "./pages/AdminPanel";
 import SearchTicket from "./pages/SearchTicket";
 import SuccessTicket from "./pages/SuccessTicket";
@@ -23,13 +25,15 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
 
-              <Route path="/login" element={<Login />} />
-
               <Route path="/new-ticket" element={<NewTicket />} />
 
               <Route path="/ticket-success" element={<SuccessTicket />} />
 
               <Route path="/ticket-search" element={<SearchTicket />} />
+
+              <Route path="/ticket/:ticketId" element={<Ticket />} />
+
+              <Route path="/login" element={<Login />} />
 
               <Route path="/admin" element={<PrivateRoute />}>
                 <Route path="/admin" element={<AdminPanel />} />
@@ -42,8 +46,6 @@ function App() {
               <Route path="/admin/ticket/:ticketId" element={<PrivateRoute />}>
                 <Route path="/admin/ticket/:ticketId" element={<Ticket />} />
               </Route>
-
-              <Route path="/ticket/:ticketId" element={<Ticket />} />
             </Routes>
           </div>
         </Router>
